20a1b7a09ee4c0d863db506fa8c81deeb7895e05
Andrii Kurdiumov <kant2002@gmail.com>
2023-06-28 20:47:45 +0600

    Add supplementary code for presentation

---
+++ 7 - troubleshooting/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Xml.Serialization;
+
+class Test
+{
+    static void Main()
+    {
+        XmlSerializer ser = new XmlSerializer(typeof(PurchaseOrder));
+        TextWriter writer = new StringWriter();
+        ser.Serialize(writer, new PurchaseOrder()
+        {
+            MyAddress = new Address() { FirstName = "Andrii" }
+        });
+        Console.WriteLine(writer.ToString());
+    }
+}
+
+public class PurchaseOrder
+{
+    public Address MyAddress;
+}
+
+public class Address
+{
+    public string FirstName;
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+MSBuild as programming language
+===============================
+
+Supplementary materials for [presentation](https://docs.google.com/presentation/d/1RWJdT_eGEpyWRPZNuwuDQyIHCG-nIo0tGa3-Naln6Ck/edit?usp=sharing) given in Jun 28 2023, Almaty.
+
